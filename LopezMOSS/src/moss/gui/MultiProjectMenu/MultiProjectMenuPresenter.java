4b14aca932e7a1c6c72ebe39f4701d88ad6a5b4d
Emil Lopez <emiljohn24@gmail.com>
2020-02-03 20:50:02 +0800

    CHANGE: Major additions in multi-project menu. See comments for change notes

--- LopezMOSS/src/moss/gui/MultiProjectMenu/MultiProjectMenuPresenter.java
+++ LopezMOSS/src/moss/gui/MultiProjectMenu/MultiProjectMenuPresenter.java
@@ -1,15 +1,99 @@
 package moss.gui.MultiProjectMenu;
 
 
+import com.airhacks.afterburner.views.FXMLView;
+import javafx.beans.property.Property;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import moss.gui.CorrelationMatrixMenu.CorrelationMatrixMenuModel;
+import moss.gui.CorrelationMatrixMenu.CorrelationMatrixMenuView;
+import moss.project.PathFilter;
+
 import javax.inject.Inject;
+import java.awt.*;
+import java.nio.file.Path;
 
 /**
  * Controls the multi-project menu
  */
 public class MultiProjectMenuPresenter {
+    @FXML
+    private RadioButton regexRadio;
+    @FXML
+    private RadioButton glubRadio;
+    @FXML
+    private TextField filterText;
+    @FXML
+    private ToggleGroup filterType;
+    @FXML
+    private ListView<String> filterListView;
+    @FXML
+    private Button startComparisonButton;
+    @FXML
+    private Label projectCountLabel;
+    @FXML
+    private Label projectPathLabel;
+    @FXML
+    private Button projectPathChooseButton;
+
+
+
     //RENAME: renamed from Controller to Presenter to avoid confusion while reading papers on FXML conventions
     @Inject
     private MultiProjectMenuService services;
     @Inject
     private MultiProjectMenuModel model;
+    @Inject
+    private CorrelationMatrixMenuModel correlationMatrixModel;
+
+
+    @FXML
+    private void initialize(){
+        //PART 1: Project path selection
+        projectPathChooseButton.setOnMouseClicked((event -> {
+            //PROJECT PATH CHOOSE BUTTON ACTION
+            Path requestedPath = services.requestDirectoryFromUser();
+            model.setChosenProjectsPath(requestedPath);
+            projectPathLabel.setText(String.valueOf(requestedPath));
+            projectCountLabel.setText(String.valueOf(services.folderCount(requestedPath)));
+            startComparisonButton.setDisable(false);
+        }));
+
+        //PART 2: Text filter
+        filterText.setOnAction((event -> {
+            filterListView.getItems().add(filterText.getText());
+            model.getFilterBuilder().addFilter(filterText.getText());
+        }));
+        //VERBOSE: This simply takes the underlying value of the newly selected radio button (Filter type) and
+        //injects that to the filter builder
+        filterType.selectedToggleProperty().addListener((observable, oldValue, newValue) ->
+                model.getFilterBuilder().setFilterType((PathFilter.Type) newValue.getUserData()));
+        regexRadio.setUserData(PathFilter.Type.REGEX);
+        glubRadio.setUserData(PathFilter.Type.GLOB);
+
+
+        //PART 3: Comparison button
+        startComparisonButton.setDisable(true);
+        startComparisonButton.setOnMouseClicked((event -> {
+            //creates the correlation matrix and loads it into the correlation matrix menu's model
+            correlationMatrixModel.loadMatrix(services.processMultiProjectFolder(model.getChosenProjectsPath(), model.createFilter()));
+
+
+            //Load correlation matrix menu
+            CorrelationMatrixMenuView matrixView = new CorrelationMatrixMenuView();
+            Stage temporaryMatrixStage = new Stage();
+            temporaryMatrixStage.setScene(new Scene(matrixView.getView()));
+            temporaryMatrixStage.show();
+        }));
+
+
+    }
 }
--- LopezMOSS/src/moss/gui/MultiProjectMenu/MultiProjectMenuService.java
+++ LopezMOSS/src/moss/gui/MultiProjectMenu/MultiProjectMenuService.java
@@ -1,22 +1,30 @@
 package moss.gui.MultiProjectMenu;
 
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 import moss.algorithm.TokenHashingStrategy;
 import moss.project.MultiProjectStorage;
 import moss.project.PathFilter;
-import moss.project.TestObjects;
 import moss.projectpairmachine.ProjectsCorrelationMatrix;
 import moss.projectpairmachine.SimpleMultiProjectComparison;
 
-import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.stream.Stream;
 
 /**
  * Services to be used by the multi-project menu GUI
  */
+@SuppressWarnings("WeakerAccess")
 public class MultiProjectMenuService {
-    @Inject
-    private final SimpleMultiProjectComparison comparisonMachine = SimpleMultiProjectComparison.fromStrategy(new TokenHashingStrategy());
+    //CHANGE: Removed @Inject for comparisonMachine because it makes no difference. Only one instance of this class will be used.
+    //A static reference will be used instead
+    private static final SimpleMultiProjectComparison comparisonMachine = SimpleMultiProjectComparison.fromStrategy(new TokenHashingStrategy());
 
     /**
      * @param projectsFolder Folder containing all the projects to be compared
@@ -29,6 +37,26 @@ public class MultiProjectMenuService {
         return comparisonMachine.compareAll(projects);
     }
 
+
+    /**
+     * @param folder Path whose subdirectories will be counted
+     * @return Number of folders/directories in the <b>folder</b> path
+     */
+    public int folderCount(Path folder){
+        int count = 0;
+        try(Stream<Path> paths = Files.list(folder)) {
+            //Removes all non-directories then returns the count of whatever remains
+            return (int) paths
+                        .filter(Files::isDirectory)
+                        .count();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+
+
     /**
      * @param paths Collection of paths to be included in the project
      * @param filters Filters to be imposed upon the projects
@@ -38,5 +66,26 @@ public class MultiProjectMenuService {
         MultiProjectStorage projects =
                 MultiProjectStorage.fromPathCollection(paths, filters);
         return comparisonMachine.compareAll(projects);
+    }
+
+
+    /**
+     * This will bring up
+     * @return Path given by the user
+     */
+    Path requestDirectoryFromUser() {
+        //TODO: Consider moving this to the multi-project menu presenter. Having a GUI-based request feels out of place here
+        //NOTE: This has been made package-private precisely because of the reasons stated above.
+        final DirectoryChooser directoryChooser = new DirectoryChooser();
+        final File requestedDirectory = directoryChooser.showDialog(new Stage());
+        //We need to check if the directory is valid
+        if (requestedDirectory != null){
+            return requestedDirectory.toPath();
+        } else{
+            //if the user fails to pick a path, the original path he came from will be returned instead
+            //this is to prevent errors from happening in the system.
+            return directoryChooser.getInitialDirectory().toPath();
+        }
+
     }
 }
