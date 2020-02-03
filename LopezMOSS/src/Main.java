923dcf7ee49ab54dbc671708ee0a8166519358b6
Emil Lopez <emiljohn24@gmail.com>
2020-02-03 20:50:35 +0800

    CHANGE: Changed main to JavaFX

--- LopezMOSS/src/Main.java
+++ LopezMOSS/src/Main.java
@@ -1,24 +1,26 @@
+import com.airhacks.afterburner.views.FXMLView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import moss.algorithm.ComparisonStrategy;
 import moss.algorithm.TokenHashingStrategy;
+import moss.gui.MultiProjectMenu.MultiProjectMenuView;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) throws IOException {
-        BufferedReader file1 = new BufferedReader(new FileReader("test_program1.txt"));
-        BufferedReader file2 = new BufferedReader(new FileReader("test_program2.txt"));
+        launch(args);
+    }
 
-        BufferedReader file1cpp = new BufferedReader(new FileReader("test_program1.cpp"));
-        BufferedReader file2cpp = new BufferedReader(new FileReader("test_program2.cpp"));
-        ComparisonStrategy comparisonAlgo = new TokenHashingStrategy();
-        ComparisonStrategy comparisonAlgo2 = new TokenHashingStrategy();
-        Double result1 = comparisonAlgo.compare(file1, file2);
-        Double result2 = comparisonAlgo2.compare(file1cpp, file2cpp);
-
-        System.out.println("Java comparison: " + result1);
-        System.out.println("C++ comparison: " + result2);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        MultiProjectMenuView startingPoint = new MultiProjectMenuView();
+        Scene startingScene = new Scene(startingPoint.getView());
+        primaryStage.setScene(startingScene);
+        primaryStage.show();
 
     }
 }
