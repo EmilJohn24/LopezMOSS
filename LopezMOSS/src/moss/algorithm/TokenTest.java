536bbe25343f4b5c29012a8259e708ea9a80e9c6
Emil Lopez <emiljohn24@gmail.com>
2020-02-21 23:49:56 +0800

    TEST: Test docs and new tests for tokens added

--- LopezMOSS/src/moss/algorithm/TokenTest.java
+++ LopezMOSS/src/moss/algorithm/TokenTest.java
@@ -1,12 +1,48 @@
 package moss.algorithm;
 
+import org.eclipse.osgi.framework.internal.core.Tokenizer;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.StringReader;
+
 class TokenTest {
-    public static void main(String[] args){
-//        Token token1 = new Token.TokenBuilder().setID(123).setLineNo(1).createToken();
-//        Token token2 = new Token.TokenBuilder().setID(1234).setLineNo(1).createToken();
-//        Token token3 = new Token.TokenBuilder().setID(1234).setLineNo(1).createToken();
-//        System.out.println(token1.hashCode());
-//        System.out.println(token2.hashCode());
-//        System.out.println(token3.hashCode());
+    private static String TEST_TOKEN_STRING_1 = "if";
+    private static String TEST_TOKEN_STRING_2 = "else";
+    private static String TEST_TOKEN_STRING_3 = "if";
+    private static String TEST_TOKENS = "if else then";
+
+    private static StringReader TEST_TOKEN_READER = new StringReader(TEST_TOKENS);
+
+    private static Token token1 = new Token.TokenBuilder().setTypeWithValue(Token.TYPE.WORD, TEST_TOKEN_STRING_1).createToken();
+    private static Token token2 = new Token.TokenBuilder().setTypeWithValue(Token.TYPE.WORD, TEST_TOKEN_STRING_2).createToken();
+    private static Token token3 = new Token.TokenBuilder().setTypeWithValue(Token.TYPE.WORD, TEST_TOKEN_STRING_3).createToken();
+
+    /**
+     * Check if tokens that have the same string content are equal
+     */
+    @Test
+    void tokenEqualityTest(){
+        assert(token1.equals(token3));
+        assert(!token1.equals(token2));
+    }
+
+
+    /**
+     * Checks if the hash value of equal tokens are equal. See the equals and hashCode equivalence contract for more details
+     */
+    @Test
+    void tokenConsistentHashTest(){
+        assert(token1.hashCode() == token3.hashCode());
+        assert(token1.hashCode() != token2.hashCode());
+    }
+
+    /**
+     * Checks if the tokenization yields the same value as the value in the reader
+     */
+    @Test
+    void tokenizationTest() throws IOException, HashingTokenizer.TokenizerEndException {
+        HashingTokenizer tokenizer = new HashingTokenizer(TEST_TOKEN_READER);
+        assert tokenizer.getNextTokenInfo().getValue().equals("else");
     }
 }
